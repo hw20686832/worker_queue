159d588b2346d24b8655ca8808bcfb3f43f11296
David Wong <stef-hw@163.com>
2013-11-21 17:09:46 +0800

    update

--- lib/solr.py
+++ lib/solr.py
@@ -3,7 +3,7 @@ import pysolr
 
 from lib.pushIndexHelper import PushIndexHelper
 
-solr = pysolr.Solr('http://localhost:8983/solr/', timeout=10)
+solr = pysolr.Solr('http://192.168.2.233:1984/solr/', timeout=10)
 helper = PushIndexHelper()
 
 def add(item):
